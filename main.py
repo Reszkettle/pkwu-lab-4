1240b328446a4afb03f041336caf140fe3e5c0f4
Rechu <reszkettle@gmail.com>
2021-11-25 12:48:59 +0100

    Created endpoint structure

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
+
+
+@app.post(path="/analyse-string")
+async def analyse_string(request: InAnalyseString):
+    output_str = process_request(request)
+    return output_str
+
+
+@app.post(path="/format-analysed-string")
+async def format_analysed_string(request: InFormatAnalysedString):
+    output_str = format_analysis(request)
+    return output_str
