d8645a4d279f27b89010200449fdea147ada6346
Rechu <reszkettle@gmail.com>
2021-11-25 12:54:28 +0100

    Prepared input schemas and service method bodies

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
+from typing import Optional
 from fastapi import FastAPI
+from dataclasses import dataclass
+from enum import Enum
+
+
+class AnalysisFormat(str, Enum):
+    JSON = "json"
+    XML = "xml"
+    CSV = "csv"
+    TEXT = "text"
+
+
+@dataclass
+class InAnalyseString:
+    firstConversionFormat: AnalysisFormat
+    outputFormat: AnalysisFormat
+    string: str
+    substring: Optional[str]
+
+
+@dataclass
+class InFormatAnalysedString:
+    inputFormat: AnalysisFormat
+    outputFormat: AnalysisFormat
+    analysis: str
+
+
+def process_request(request: InAnalyseString):
+    pass
+
+
+def format_analysis(request: InFormatAnalysedString):
+    pass
 
 
 app = FastAPI()
